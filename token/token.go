e8235a4db9d6627a6cc0b6475294b41e0d5a080a
Nikolay Tuzov <n.tuzov@gaijin.ru>
2019-10-15 22:59:13 +0300

    [token] added package token

---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+// FEATURE: int or byte can provide better performance
+type Type string
+
+// FEATURE: save line, column, filename (useful for debug)
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+// Token types
+const (
+	// Special types
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
